f660ea43bd9d2462e6b8486985d67ba9a328278a
Sahar Ali <sahar.aliabdou@gmail.com>
2022-03-28 10:28:22 +0200

    Added class table.

--- Assignment_A1_01/Program.cs
+++ Assignment_A1_01/Program.cs
@@ -18,10 +18,44 @@ namespace Assignment_A1_01
         {
             double latitude = 59.5086798659495;
             double longitude = 18.2654625932976;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
 
-            var t1 = new OpenWeatherService().GetForecastAsync(latitude, longitude);
-            
+
+            //var t1 = new OpenWeatherService().GetForecastAsync(latitude, longitude);
+
             //Your Code
+
+            OpenWeatherService service = new OpenWeatherService();
+
+            Task<Forecast> t1 = service.GetForecastAsync(latitude, longitude);
+            var table = new Table();
+            
+            Task.WaitAll(t1);
+
+            //Console.WriteLine("-----------------");
+            if (t1?.Status == TaskStatus.RanToCompletion)
+            {
+                Forecast forecast = t1.Result;
+                //Console.WriteLine($"Weather forecast for {forecast.City}");
+                table.SetHeaders($"weather Forecast for {forecast.City}");
+                var GroupedList = forecast.Items.GroupBy(item => item.DateTime.Date);
+
+                foreach (IGrouping<DateTime, ForecastItem> group in GroupedList)
+                {
+                    table.AddRow(group.Key.Date.ToShortDateString());
+                    foreach (ForecastItem item in group)
+                    {
+                        table.AddRow($"   - {item.DateTime.ToShortTimeString()}: {item.Description},Temperature: {item.Temperature} , Wind: {item.WindSpeed} m/s");
+                    }
+                }
+                Console.WriteLine(table.ToString());
+
+            }
+            else
+            {
+                Console.WriteLine($"Geolocation weather service error.");
+            }
+
         }
     }
 }
--- Assignment_A1_01/Services/OpenWeatherService.cs
+++ Assignment_A1_01/Services/OpenWeatherService.cs
@@ -14,21 +14,47 @@ namespace Assignment_A1_01.Services
     public class OpenWeatherService
     {
         HttpClient httpClient = new HttpClient();
-        readonly string apiKey = ""; // Your API Key
+        readonly string apiKey = "7616850dd80ab0c5df57eb53e6668a2d"; // Your API Key
         public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
         {
             //https://openweathermap.org/current
             var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
-            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
-
+            //var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?" +
+            $"lat={latitude}" +
+            $"&lon={longitude}" +
+            $"&units=metric" +
+            $"&lang={language}" +
+            $"&appid={apiKey}";
             //Read the response from the WebApi
             HttpResponseMessage response = await httpClient.GetAsync(uri);
             response.EnsureSuccessStatusCode();
             WeatherApiData wd = await response.Content.ReadFromJsonAsync<WeatherApiData>();
 
             //Your Code to convert WeatherApiData to Forecast using Linq.
+            Forecast forecast = new Forecast();
+
+            forecast.City = wd.city.name;
+
+
+            forecast.Items = new List<ForecastItem>();
+
+            wd.list.ForEach(wdListItem => { forecast.Items.Add(GetForecastItem(wdListItem)); });
 
             return forecast;
+        }
+
+        private ForecastItem GetForecastItem(List wdListItem)
+        {
+
+            ForecastItem item = new ForecastItem();
+            item.DateTime = UnixTimeStampToDateTime(wdListItem.dt);
+
+            item.Temperature = wdListItem.main.temp;
+            item.Description = wdListItem.weather.Count > 0 ? wdListItem.weather.First().description : "No info";
+            item.WindSpeed = wdListItem.wind.speed;
+
+            return item;
         }
         private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
